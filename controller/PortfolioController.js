a07ebc071c99c0b60263b08a5e6dbfc157fc180e
Neeraj kumar <neeraj100296@gmail.com>
2025-04-05 22:15:22 +0530

    refactor: update VideoType enum and modify portfolio table type column

--- controller/PortfolioController.js
+++ controller/PortfolioController.js
@@ -2,147 +2,150 @@ import prisma from "../DB/db.config.js";
 
 // Fetch All Portfolio Data
 export const fetchAllPortfolioData = async (req, res) => {
-    try {
-      const portfolioData = await prisma.portfolio.findMany({
-        select: {
-          id: true,
-          title: true,
-          description: true,
-          type: true,
-          videoId: true,
-          isLive: true,
-          creator: {
-            select: {
-              id: true,
-              name: true,
-              email: true,
-              phone: true,
-            },
+  try {
+    const portfolioData = await prisma.portfolio.findMany({
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        type: true,
+        videoId: true,
+        isLive: true,
+        creator: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            phone: true,
           },
         },
-      });
-      return res.status(200).json({
-        status: 200,
-        data: portfolioData,
-        msg: "Fetched all portfolio data",
-      });
-    } catch (error) {
-      console.error("Error fetching portfolio data:", error);
-      return res.status(500).json({
-        status: 500,
-        msg: "Failed to fetch portfolio data",
-        error: error.message,
-      });
-    }
-  };
-  
-  // Get Portfolio Details by ID
-  export const getPortfolioDetails = async (req, res) => {
-    const { id } = req.params;
-  
-    try {
-      const portfolio = await prisma.portfolio.findUnique({
-        where: { id: Number(id) },
-        select: {
-          id: true,
-          title: true,
-          description: true,
-          type: true,
-          videoId: true,
-          isLive: true,
-          creator: {
-            select: {
-              id: true,
-              name: true,
-              email: true,
-              phone: true,
-            },
+      },
+    });
+    return res.status(200).json({
+      status: 200,
+      data: portfolioData,
+      msg: "Fetched all portfolio data",
+    });
+  } catch (error) {
+    console.error("Error fetching portfolio data:", error);
+    return res.status(500).json({
+      status: 500,
+      msg: "Failed to fetch portfolio data",
+      error: error.message,
+    });
+  }
+  finally {
+    await prisma.$disconnect();
+  }
+};
+
+// Get Portfolio Details by ID
+export const getPortfolioDetails = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const portfolio = await prisma.portfolio.findUnique({
+      where: { id: Number(id) },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        type: true,
+        videoId: true,
+        isLive: true,
+        creator: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            phone: true,
           },
         },
-      });
-  
-      if (!portfolio) {
-        return res.status(404).json({
-          status: 404,
-          msg: "Portfolio not found",
-        });
-      }
-  
-      return res.status(200).json({
-        status: 200,
-        data: portfolio,
-        msg: "Fetched portfolio details",
-      });
-    } catch (error) {
-      console.error("Error fetching portfolio details:", error);
-      return res.status(500).json({
-        status: 500,
-        msg: "Failed to fetch portfolio details",
-        error: error.message,
+      },
+    });
+
+    if (!portfolio) {
+      return res.status(404).json({
+        status: 404,
+        msg: "Portfolio not found",
       });
     }
-  };
-  
+
+    return res.status(200).json({
+      status: 200,
+      data: portfolio,
+      msg: "Fetched portfolio details",
+    });
+  } catch (error) {
+    console.error("Error fetching portfolio details:", error);
+    return res.status(500).json({
+      status: 500,
+      msg: "Failed to fetch portfolio details",
+      error: error.message,
+    });
+  }
+};
+
 
 // Add New Portfolio
 export const createNewPortfolio = async (req, res) => {
-    const { title, description, type, youtubeLink, isLive, creator } = req.body;
-  
-    if (!title || !description || !type) {
-      return res.status(400).json({
-        status: 400,
-        msg: "Title, description, and type are required fields",
-      });
-    }
-  
-    try {
-      // Check if the creator exists
-      const existingCreator = await prisma.user.findUnique({
-        where: { id: creator },
-      });
-  
-      if (!existingCreator) {
-        return res.status(404).json({
-          status: 404,
-          msg: "User does not exist",
-        });
-      }
+  const { title, description, type, youtubeLink, isLive, creator } = req.body;
 
-      const videoId = getYouTubeVideoId(youtubeLink);
-        
-      // Validate the extracted ID
-      if (youtubeLink && !videoId) {
-          return res.status(400).json({ status: 400, msg: "Invalid YouTube link" });
-      }
-  
-      const newPortfolio = await prisma.portfolio.create({
-        data: {
-          title,
-          description,
-          type,
-          videoId,
-          isLive,
-          creator: {
-            connect: { id: creator }, // Link to an existing user with the given ID
-          },
-        },
-      });
-  
-      return res.status(201).json({
-        status: 201,
-        data: newPortfolio,
-        msg: "New Portfolio Created",
-      });
-    } catch (error) {
-      console.error("Error creating new portfolio:", error);
-      return res.status(500).json({
-        status: 500,
-        msg: "Failed to create new portfolio",
-        error: error.message,
+  if (!title || !description || !type) {
+    return res.status(400).json({
+      status: 400,
+      msg: "Title, description, and type are required fields",
+    });
+  }
+
+  try {
+    // Check if the creator exists
+    const existingCreator = await prisma.user.findUnique({
+      where: { id: creator },
+    });
+
+    if (!existingCreator) {
+      return res.status(404).json({
+        status: 404,
+        msg: "User does not exist",
       });
     }
-  };
-  
+
+    const videoId = getYouTubeVideoId(youtubeLink)
+
+    // Validate the extracted ID
+    if (youtubeLink && !videoId) {
+      return res.status(400).json({ status: 400, msg: "Invalid YouTube link" });
+    }
+
+    const newPortfolio = await prisma.portfolio.create({
+      data: {
+        title,
+        description,
+        type,
+        videoId: videoId || '',
+        isLive,
+        creator: {
+          connect: { id: creator }, // Link to an existing user with the given ID
+        },
+      },
+    });
+
+    return res.status(201).json({
+      status: 201,
+      data: newPortfolio,
+      msg: "New Portfolio Created",
+    });
+  } catch (error) {
+    console.error("Error creating new portfolio:", error);
+    return res.status(500).json({
+      status: 500,
+      msg: "Failed to create new portfolio",
+      error: error.message,
+    });
+  }
+};
+
 
 
 
@@ -150,75 +153,75 @@ export const createNewPortfolio = async (req, res) => {
 
 // Update Portfolio by ID
 export const updatePortfolio = async (req, res) => {
-    const { id } = req.params;
-    const { title, description, type, youtubeLink, isLive, creator } = req.body;
-  
-    try {
-      // Check if the portfolio exists
-      const existingPortfolio = await prisma.portfolio.findUnique({
-        where: { id: Number(id) },
-      });
-  
-      if (!existingPortfolio) {
-        return res.status(404).json({
-          status: 404,
-          msg: `Portfolio not found`,
-        });
-      }
-  
-      // Check if the updater (creator) exists
-      if (creator) {
-        const existingCreator = await prisma.user.findUnique({
-          where: { id: creator },
-        });
-  
-        if (!existingCreator) {
-          return res.status(404).json({
-            status: 404,
-            msg: "User does not exist",
-          });
-        }
-      }
-  
-      const videoId = getYouTubeVideoId(youtubeLink);
-        
-      // Validate the extracted ID
-      if (youtubeLink && !videoId) {
-          return res.status(400).json({ status: 400, msg: "Invalid YouTube link" });
-      }
+  const { id } = req.params;
+  const { title, description, type, youtubeLink, isLive, creator } = req.body;
 
-      const updatedPortfolio = await prisma.portfolio.update({
-        where: { id: Number(id) },
-        data: {
-          title: title || existingPortfolio.title,
-          description: description || existingPortfolio.description,
-          type: type || existingPortfolio.type,
-          videoId: videoId || existingPortfolio.videoId,
-          isLive: typeof isLive === "boolean" ? isLive : existingPortfolio.isLive,
-          updater: creator
-            ? {
-                connect: { id: creator }, // Link to an existing user with the given ID
-              }
-            : undefined, // Only update if creator is provided
-        },
-      });
-  
-      return res.status(200).json({
-        status: 200,
-        data: updatedPortfolio,
-        msg: "Portfolio updated successfully",
-      });
-    } catch (error) {
-      console.error("Error updating portfolio:", error);
-      return res.status(500).json({
-        status: 500,
-        msg: "Failed to update portfolio",
-        error: error.message,
+  try {
+    // Check if the portfolio exists
+    const existingPortfolio = await prisma.portfolio.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!existingPortfolio) {
+      return res.status(404).json({
+        status: 404,
+        msg: `Portfolio not found`,
       });
     }
-  };
 
-  
+    // Check if the updater (creator) exists
+    if (creator) {
+      const existingCreator = await prisma.user.findUnique({
+        where: { id: creator },
+      });
+
+      if (!existingCreator) {
+        return res.status(404).json({
+          status: 404,
+          msg: "User does not exist",
+        });
+      }
+    }
+
+    const videoId = getYouTubeVideoId(youtubeLink);
+
+    // Validate the extracted ID
+    if (youtubeLink && !videoId) {
+      return res.status(400).json({ status: 400, msg: "Invalid YouTube link" });
+    }
+
+    const updatedPortfolio = await prisma.portfolio.update({
+      where: { id: Number(id) },
+      data: {
+        title: title || existingPortfolio.title,
+        description: description || existingPortfolio.description,
+        type: type || existingPortfolio.type,
+        videoId: videoId || existingPortfolio.videoId,
+        isLive: typeof isLive === "boolean" ? isLive : existingPortfolio.isLive,
+        updater: creator
+          ? {
+            connect: { id: creator }, // Link to an existing user with the given ID
+          }
+          : undefined, // Only update if creator is provided
+      },
+    });
+
+    return res.status(200).json({
+      status: 200,
+      data: updatedPortfolio,
+      msg: "Portfolio updated successfully",
+    });
+  } catch (error) {
+    console.error("Error updating portfolio:", error);
+    return res.status(500).json({
+      status: 500,
+      msg: "Failed to update portfolio",
+      error: error.message,
+    });
+  }
+};
+
+
 
 
 
@@ -261,14 +264,14 @@ export const deletePortfolio = async (req, res) => {
 
 const getYouTubeVideoId = (url) => {
   try {
-      const parsedUrl = new URL(url);
-      if (parsedUrl.hostname.includes("youtube.com")) {
-          return parsedUrl.searchParams.get("v"); // Extract from "v" parameter
-      }
-      if (parsedUrl.hostname.includes("youtu.be")) {
-          return parsedUrl.pathname.substring(1); // Extract from path
-      }
+    const parsedUrl = new URL(url);
+    if (parsedUrl.hostname.includes("youtube.com")) {
+      return parsedUrl.searchParams.get("v"); // Extract from "v" parameter
+    }
+    if (parsedUrl.hostname.includes("youtu.be")) {
+      return parsedUrl.pathname.substring(1); // Extract from path
+    }
   } catch (error) {
-      return null; // Return null if URL is invalid
+    return null; // Return null if URL is invalid
   }
 };
